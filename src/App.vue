8c780757ca69e17517de894365d040c24d2ead2d
Priyo Pujonggo <prio.pujonggo@gmail.com>
2022-04-13 04:20:59 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="py-16 white overflow-y-auto">  
+    <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+        <!-- <h2 class="mb-12 text-center text-2xl text-gray-900 font-bold md:text-4xl">What's our customers say</h2> -->
+        <div class="grid gap-8 md:grid-rows-2 lg:grid-cols-2">
+          <div class="row-span-2 p-6 rounded-lg text-center sm:p-8">
+            <div class="h-full flex flex-col justify-center space-y-4">
+              <img class="mx-auto rounded-full border-4 border-indigo-500" src="/assets/img/prio.png" alt="user avatar" height="220" width="220" loading="lazy">
+              <h2 class="text-white text-lg font-bold text-justify">Hello, My name is Priyo Pujonggo</h2>
+              <p class="text-white md:text-xl text-left"> <span class="font-serif"></span> I am a Full Stack Developer, Professionally I have been working in the field of Web Development using Laravel, Vue js, Bootstrap, and MySQL. Beside that I'm currently learning Python especially FastAPI framework, Svelte , tailwind CSS PostgreSQL and Mongo DB. In my spare time I love to read about Philosophy, Technology and programming things.<span class="font-serif"></span></p>
+              <div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row-span-2 p-6 rounded-lg text-center sm:p-8">
+            <img class="mx-auto w-full md:w-4/5 transform -rotate-6 transition hover:scale-105 duration-700 ease-in-out hover:rotate-6 bg-gradient-to-r from-grey-900 via-blue-500 to-purple-500" src="/assets/img/logo_job.png" />
+          </div>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+  name: "App",
+  components: {
+  }
+}
+</script>
+
